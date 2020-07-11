710f97c4990cae553d67fa56b68456aecce23a82
manish shah <me.lekha7759@gmail.com>
2020-07-11 14:36:09 +0545

    Added product and category . Also Added Database in our project using entity frame work ...

---
+++ ClothBazar.DataBase/CBContext.cs
@@ -0,0 +1,21 @@
+﻿using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.DataBase
+{
+    public class CBContext:DbContext
+    {
+        public CBContext():base("ClothBazarConnection")
+        {
+
+        }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Category> categories { get; set; }
+
+    }
+}
